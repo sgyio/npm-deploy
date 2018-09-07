bc10c14d4b69eca74e7c6518d03264c0b3606c4c
Stéphane Gariépy <sgariepy@gmail.com>
2018-09-06 23:17:13 -0400

    warning message

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # npm-deploy (aka Hello-World)
 
 Test package on npm to automatically deploy with 2FA enabled for publish.
+
+Do not npm install this package, it's useless.
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
 
-totp=$(curl -s -X GET $VAULT_URL -H "X-Vault-Token: ${VAULT_NPM_TOTP_ACCESS_TOKEN}" | jq --raw-output ".data.code")
+totp=$(curl -s -X GET $VAULT_URL -H "X-Vault-Token: ${VAULT_NPM_TOTP_TOKEN}" | jq --raw-output ".data.code")
 
 npm publish . --otp=$totp
