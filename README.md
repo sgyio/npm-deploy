9065116aef679d5450af7e82733b3c6cdf23a912
Stéphane Gariépy <sgariepy@gmail.com>
2018-09-06 23:00:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# npm-deploy (aka Hello-World)
+
+Test package on npm to automatically deploy with 2FA enabled for publish.
---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
+
+totp=$(curl -s -X GET $VAULT_URL -H "X-Vault-Token: ${VAULT_NPM_TOTP_ACCESS_TOKEN}" | jq --raw-output ".data.code")
+
+npm publish . --otp=$totp
