6c6d5d0d39813b222b1d95d7995a4962ada2d6c3
Stéphane Gariépy <sgariepy@gmail.com>
2019-06-27 17:02:34 -0400

    chore: minor changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "//registry.npmjs.org/:_authToken=\${NPM_TOKEN}" > .npmrc
-
 totp=$(curl -s -X GET $VAULT_URL -H "X-Vault-Token: ${VAULT_NPM_TOTP_TOKEN}" | jq --raw-output ".data.code")
-
 npm publish . --otp=$totp
